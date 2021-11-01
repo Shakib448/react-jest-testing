6cea75a1eccc88251194cbacc69c97e1a2dd5c1c
Shakiba448 <shakiba448@gmail.com>
2021-11-01 18:46:11 +0600

    Testing with jest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("red");
+  const newButtonColor = color === "red" ? "blue" : "red";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        style={{ backgroundColor: color }}
+        onClick={() => setColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input type="checkbox" />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("testing color button", () => {
+  let colorButton;
+  let checkBox;
+  beforeAll(() => {
+    render(<App />);
+    colorButton = screen.getByRole("button", { name: /change to blue/i });
+    checkBox = screen.getByRole("checkbox");
+  });
+
+  it("button has correct initial color", () => {
+    expect(colorButton).toHaveStyle({
+      backgroundColor: "red",
+    });
+
+    fireEvent.click(colorButton);
+
+    expect(colorButton).toHaveStyle({
+      backgroundColor: "blue",
+    });
+
+    expect(colorButton.textContent).toBe("Change to red");
+  });
+
+  test("initial conditions", () => {
+    expect(colorButton).toBeEnabled();
+    expect(checkBox).not.toBeChecked();
+  });
 });
