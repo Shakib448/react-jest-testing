cab16dd06ea39018026384da16baeeeecfcf24ea
Shakiba448 <shakiba448@gmail.com>
2021-11-01 20:59:43 +0600

    WIP on husky

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css'
-
 import React, { useState } from 'react'
 
 function App() {
-    const [color, setColor] = useState('red')
-    const newButtonColor = color === 'red' ? 'blue' : 'red'
-    return (
-        <div className="App">
-            <button
-                style={{ backgroundColor: color }}
-                onClick={() => setColor(newButtonColor)}
-            >
-                Change to {newButtonColor}
-            </button>
-            <input type="checkbox" />
-        </div>
-    )
+  const [color, setColor] = useState('red')
+  const newButtonColor = color === 'red' ? 'blue' : 'red'
+  return (
+    <div className="App">
+      <button
+        style={{ backgroundColor: color }}
+        onClick={() => setColor(newButtonColor)}
+      >
+        Change to {newButtonColor}
+      </button>
+      <input type="checkbox" />
+    </div>
+  )
 }
 
 export default App
