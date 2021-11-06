1e456f4d644fd16d7f2ba6571c25e44c8af14e67
Shakiba448 <shakiba448@gmail.com>
2021-11-06 10:08:26 +0600

    WIP on testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import SummaryForm from "../src/pages/summary/SummaryForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SummaryForm />
+    </div>
+  );
 }
 
 export default App;
